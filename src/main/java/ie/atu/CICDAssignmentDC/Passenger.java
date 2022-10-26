c53bfef3816a7c66943574d72c49ec2958a06de0
Denis Costello <G00396309@atu.ie>
2022-10-26 14:16:01 +0100

    test Passenger

--- src/main/java/ie/atu/CICDAssignmentDC/Passenger.java
+++ src/main/java/ie/atu/CICDAssignmentDC/Passenger.java
@@ -1,4 +1,34 @@
 package ie.atu.CICDAssignmentDC;
 
 public class Passenger {
+    private String Title;
+    private String Name;
+    private String ID;
+    private int Phone;
+    private int Age;
+
+    public void setTitle(String title) {
+        if("Mr".equalsIgnoreCase(title) || "Mrs".equalsIgnoreCase(title) ||"Ms".equalsIgnoreCase(title)) {
+            Title = title;
+        }
+        else{
+            System.out.println("Please enter ");
+        }
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
 }
--- src/test/java/ie/atu/CICDAssignmentDC/PassengerTest.java
+++ src/test/java/ie/atu/CICDAssignmentDC/PassengerTest.java
@@ -2,15 +2,23 @@ package ie.atu.CICDAssignmentDC;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+    Passenger pass1;
 
     @BeforeEach
     void setUp() {
+        pass1 = new Passenger(Mr, Denis, 1234567891,0873928961,20);
     }
 
+    @Test
+    void testTitle(){
+        assertEquals();
+
+    }
     @AfterEach
     void tearDown() {
     }
